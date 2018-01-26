e98c8d59f3c2832f6c8c9a82c3e2f29236be18fe
ThomasBardet <bardet.thomas@gmail.com>
2018-01-27 00:38:15 +0100

    first player iteration

---
+++ Migration/Assets/Sources/Script/MovementBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    PlayerVariables birdCaracteristics;
+    
+    public float joystick;
+
+    Rigidbody playerRb;
+	// Use this for initialization
+	void Start ()
+    {
+        playerRb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        InputGetter();
+        JoystickOrientation();
+
+        joystick = Input.GetAxis("LeftJoystick X");
+	}
+
+    private void FixedUpdate()
+    {
+        MoveForward();
+
+    }
+
+    void InputGetter()
+    {
+        if (Input.GetButtonDown("Button A"))
+        {
+            FlappingWings();
+        }
+        
+    }
+
+    void JoystickOrientation()
+    {
+        MoveTranslate(Input.GetAxis("LeftJoystick X"));
+
+        
+    }
+
+    void MoveForward()
+    {
+        playerRb.AddForce(transform.up * birdCaracteristics.speed, ForceMode.Impulse);
+        Debug.DrawRay(transform.position, transform.up * birdCaracteristics.speed, Color.red);
+    }
+
+    void MoveTranslate(float joystickValue)
+    {
+       
+        playerRb.AddTorque(transform.right * birdCaracteristics.turnSpeed * joystick, ForceMode.Force);
+        playerRb.AddTorque(-transform.up * birdCaracteristics.turnSpeed* joystick);
+    }
+
+    void FlappingWings()
+    {
+        playerRb.AddForce(Vector3.up * birdCaracteristics.flapForce, ForceMode.Impulse);
+
+    }
+}
---
+++ Migration/Assets/Sources/Script/PlayerVariables.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerVariables", menuName = "3C")]
+public class PlayerVariables : ScriptableObject {
+
+    public float speed;
+    public float flapForce;
+    public float turnSpeed;
+    public float JoystickDeadZoneX;
+        
+}
